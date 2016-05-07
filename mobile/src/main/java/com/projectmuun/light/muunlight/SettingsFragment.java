ca7a41dffbf0c47571c5c9354d28d04d0750a78d
Micheal <wizardofnorth@bitbucket.org>
2016-05-06 22:50:54 -0700

    Cleaned up the horror in settings fragment. Loading commented out becuase it causes it to crash. Apperently it is telling me I am casting long to integer, but I am not.....

--- mobile/src/main/java/com/projectmuun/light/muunlight/SettingsFragment.java
+++ mobile/src/main/java/com/projectmuun/light/muunlight/SettingsFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,9 +21,26 @@ import android.widget.Toast;
  * Created by Micheal Roslikov.
  */
 public class SettingsFragment extends Fragment{
+
+    public static final String WAKE_MARGIN_ID = "wake_margin";
+    public static final long WAKE_MARGIN_DEFAULT = 15L  * 60 * 1000;
+    public static final int WAKE_MARGIN_SEEKBAR_POSTION_DEFUALT = 15;
+    public static final String INTERVAL_ID = "interval";
+    public static final long INTERVAL_DEFAULT = -1L;
+    public static final String INTERVAL_SPINNER_POSITION_ID = "interval_spinner_position";
+    public static final int INTERVAL_SPINNER_POSITION_DEFAULT = 0;
+    public static final String ALARM_TURN_OFF_AUTOMATICALLY_ID = "turn_off_alarm_automatically";
+    public static final boolean ALARM_TURN_OFF_AUTOMATICALLY_DEFAULT = false;
+
     SeekBar marginSeek;
     CheckBox alarmTurnOffCheckBox;
     Spinner spinner;
+
+    long wake_margin = WAKE_MARGIN_DEFAULT;
+
+    long interval = INTERVAL_DEFAULT;
+    boolean alarm_turn_off_auto = ALARM_TURN_OFF_AUTOMATICALLY_DEFAULT;
+    int spinner_position = INTERVAL_SPINNER_POSITION_DEFAULT;
 
     //Parent View
     private View myFragmentView;
@@ -44,10 +62,19 @@ public class SettingsFragment extends Fragment{
 
         marginSeek = (SeekBar) myFragmentView.findViewById(R.id.margin_seek);
         alarmTurnOffCheckBox = (CheckBox) myFragmentView.findViewById(R.id.AlarmTurnOffCheckBox);
+
+        alarmTurnOffCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                alarm_turn_off_auto = isChecked;
+            }
+        });
+
         marginSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 ((TextView) myFragmentView.findViewById(R.id.margin_seek_txt)).setText("" + progress);
+                wake_margin = progress * 60 * 1000;
             }
 
             @Override
@@ -74,26 +101,24 @@ public class SettingsFragment extends Fragment{
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch (position) {
                     case 0:
-                        updateSettings(-2l, -1, true);
+                        interval = -1L;
                         break;
                     case 1:
-                        updateSettings(-2l, 7*24*60*60*1000l, true);
+                        interval = 7 * 24 * 60 * 60 * 1000l;
                         break;
                     case 2:
-                        updateSettings(-2l, AlarmManager.INTERVAL_DAY, true);
+                        interval = AlarmManager.INTERVAL_DAY;
                         break;
                     case 3:
-                        updateSettings(-2l, AlarmManager.INTERVAL_HALF_DAY, true);
+                        interval = AlarmManager.INTERVAL_HALF_DAY;
                         break;
                     case 4:
-                        updateSettings(-2l, AlarmManager.INTERVAL_HOUR, true);
-                        break;
-                    case 5:
-                        updateSettings(-2l, AlarmManager.INTERVAL_HALF_HOUR, true);
+                        interval = AlarmManager.INTERVAL_HOUR;
                         break;
 
 
                 }
+                spinner_position = position;
             }
 
             @Override
@@ -102,21 +127,59 @@ public class SettingsFragment extends Fragment{
             }
         });
 
+        (myFragmentView.findViewById(R.id.save_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSettings();
+            }
+        });
+        (myFragmentView.findViewById(R.id.reset_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restoreSettings();
+            }
+        });
+
+//        loadSettings();
+
 
   
         return myFragmentView;
     }
     //Save the settings
-    public void updateSettings(long margin, long interval, boolean turnOffAlarmAfterStandUp) {
+    public void saveSettings() {
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext()).edit();
-        if (margin != -2l)
-            editor.putLong("Interval", interval);
-        if (margin != -2l)
-            editor.putLong("WakeMargin", margin);
 
-        editor.putBoolean("TurnOffAlarmAutomatically", turnOffAlarmAfterStandUp);
+        editor.putLong(INTERVAL_ID, interval);
+        editor.putLong(WAKE_MARGIN_ID, wake_margin);
+
+        editor.putInt(INTERVAL_SPINNER_POSITION_ID, spinner_position);
+
+        editor.putBoolean(ALARM_TURN_OFF_AUTOMATICALLY_ID, alarm_turn_off_auto);
         editor.apply();
 
         Toast.makeText(getActivity().getApplicationContext(), "Settings Saved", Toast.LENGTH_SHORT).show();
+    }
+    public void restoreSettings() {
+        marginSeek.setProgress(WAKE_MARGIN_SEEKBAR_POSTION_DEFUALT);
+        wake_margin = WAKE_MARGIN_SEEKBAR_POSTION_DEFUALT;
+
+        spinner.setSelection(INTERVAL_SPINNER_POSITION_DEFAULT,true);
+        spinner_position = INTERVAL_SPINNER_POSITION_DEFAULT;
+
+        alarmTurnOffCheckBox.setChecked(ALARM_TURN_OFF_AUTOMATICALLY_DEFAULT);
+        alarm_turn_off_auto = ALARM_TURN_OFF_AUTOMATICALLY_DEFAULT;
+
+        saveSettings();
+
+    }
+    //Retrieve the settings
+    public void loadSettings(){
+        SharedPreferences s = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        marginSeek.setProgress(s.getInt(WAKE_MARGIN_ID, 15));
+        spinner.setSelection(s.getInt(INTERVAL_SPINNER_POSITION_ID, INTERVAL_SPINNER_POSITION_DEFAULT), true);
+        alarmTurnOffCheckBox.setChecked(s.getBoolean(ALARM_TURN_OFF_AUTOMATICALLY_ID,ALARM_TURN_OFF_AUTOMATICALLY_DEFAULT));
+
     }
 }
